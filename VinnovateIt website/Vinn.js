7c091c93445b523af70304417b928dff0fc1bf9c
Atharv Shivarkar <68496002+AtharvShivarkar@users.noreply.github.com>
2020-07-25 12:41:31 +0530

    Add files via upload

---
+++ VinnovateIt website/Vinn.js
@@ -0,0 +1,60 @@
+var android = document.querySelector(".android");
+android.addEventListener("mouseover",function(){
+    android.style.color = "green";
+});
+android.addEventListener("mouseout",function(){
+    android.style.color = "black";
+});
+var web = document.querySelector(".web");
+web.addEventListener("mouseover",function(){
+    web.style.color = "rgb(5, 250, 209)";
+});
+web.addEventListener("mouseout",function(){
+    web.style.color = "black";
+});
+var iot = document.querySelector(".iot");
+iot.addEventListener("mouseover",function(){
+    iot.style.color = "rgb(255, 97, 197)";
+});
+iot.addEventListener("mouseout",function(){
+    iot.style.color = "black";
+});
+var diamond = document.querySelector(".diamond");
+diamond.addEventListener("mouseover",function(){
+    diamond.style.color  = "rgb(252, 3, 48)";
+});
+diamond.addEventListener("mouseout",function(){
+    diamond.style.color = "black";
+});
+var facebook = document.querySelector(".facebook");
+facebook.addEventListener("mouseover",function(){
+    facebook.style.color="rgb(0, 101, 242)";
+});
+facebook.addEventListener("mouseout",function(){
+    facebook.style.color ="black";
+});
+var insta = document.querySelector(".insta");
+insta.addEventListener("mouseover",function(){
+    insta.style.color = "rgb(242, 0, 149)";
+});
+insta.addEventListener("mouseout",function(){
+    insta.style.color = "black";
+});
+var linkedin = document.querySelector(".linkedin");
+linkedin.addEventListener("mouseover",function(){
+    linkedin.style.color="rgb(32, 0, 242)";
+});
+linkedin.addEventListener("mouseout",function(){
+    linkedin.style.color="black";
+});
+var github = document.querySelector(".github");
+github.addEventListener("mouseover",function(){
+    github.style.color="purple";
+});
+github.addEventListener("mouseout",function(){
+    github.style.color="black";
+})
+
+
+
+
